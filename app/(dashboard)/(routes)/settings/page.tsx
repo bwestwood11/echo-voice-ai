f2b26ab254b4c8d5d0b81398e453a96498a823e0
bwestwood11 <dabrettwestwood@gmail.com>
2023-08-08 01:00:37 -0400

    settings page update

--- app/(dashboard)/(routes)/settings/page.tsx
+++ app/(dashboard)/(routes)/settings/page.tsx
@@ -1,13 +1,16 @@
-import { checkSubscription } from "@/lib/subscription"
+import { checkSubscription, subscriptionData } from "@/lib/subscription"
 import { cn } from "@/lib/utils"
 import { Settings } from "lucide-react"
 import { SubscriptionButton } from "@/components/subscription-button"
 import { getServerSession } from "next-auth"
 import { authOptions } from "@/app/api/auth/[...nextauth]/route"
+import { Badge } from "@/components/ui/badge"
 
 const SettingsPage = async () => {
     const isPro = await checkSubscription()
     const session = await getServerSession(authOptions)
+   const subscription = await subscriptionData()
+  console.log("single subscription", subscription)
   return (
     <div>
           <div className="px-4 lg:px-8 flex items-center gap-x-3 mb-8">
@@ -24,9 +27,39 @@ const SettingsPage = async () => {
       <div className="px-4 lg:px-8 space-y-4">
        <div className="text-gray-500 text-sm">
       {isPro ? "You are currently on a pro plan." : "You are currently on a free plan."}
+     
        </div>
        <SubscriptionButton isPro={isPro} />
       </div>
+      <div>
+      <div className="mt-6 px-4 lg:px-8 border-t border-gray-100">
+        <dl className="divide-y divide-gray-100">
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">Full name</dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{session?.user.name}</dd>
+          </div>
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">Email Address</dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{session?.user.email}</dd>
+          </div>
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">Plan</dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{isPro ? "Premium Plan": "Free Plan"} <Badge className="ml-4 bg-green-600">{isPro ? "Active": "Not Active"}</Badge></dd>
+          </div>
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">Renewal Date</dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{new Date(subscription?.stripeCurrentPeriodEnd).toLocaleString()}</dd>
+          </div>
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">Renewal Amount</dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              {isPro ? "$19.99": "$0"}
+            </dd>
+          </div>
+        </dl>
+      </div>
+    </div>
+
     </div>
   
   )
--- lib/subscription.ts
+++ lib/subscription.ts
@@ -6,30 +6,52 @@ import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 const DAY_IN_MS = 86_400_000;
 
 export const checkSubscription = async () => {
-   const session = await getServerSession(authOptions);
+  const session = await getServerSession(authOptions);
 
-   if (!session?.user.id || !session) {
-      return false;
-   }
+  if (!session?.user.id || !session) {
+    return false;
+  }
 
-   const userSubscription = await prismadb.userSubscription.findUnique({
-        where: {
-            userId: session.user.id
-        },
-        select: {
-            stripeSubscriptionId: true,
-            stripeCurrentPeriodEnd: true,
-            stripeCustomerId: true,
-            stripePriceId: true,
-        },
-    });
-   
-    if (!userSubscription) {
-        return false;
-    }
+  const userSubscription = await prismadb.userSubscription.findUnique({
+    where: {
+      userId: session.user.id,
+    },
+    select: {
+      stripeSubscriptionId: true,
+      stripeCurrentPeriodEnd: true,
+      stripeCustomerId: true,
+      stripePriceId: true,
+    },
+  });
 
-    const isValid = userSubscription.stripePriceId && 
-    userSubscription.stripeCurrentPeriodEnd?.getTime()! + DAY_IN_MS > Date.now();
+  if (!userSubscription) {
+    return false;
+  }
 
-    return !!isValid;
+  const isValid =
+    userSubscription.stripePriceId &&
+    userSubscription.stripeCurrentPeriodEnd?.getTime()! + DAY_IN_MS >
+      Date.now();
+
+  return !!isValid;
+};
+
+export const subscriptionData = async () => {
+  const session = await getServerSession(authOptions);
+
+  if (!session?.user.id || !session) {
+    return null;
+  }
+
+  const userSubscription = await prismadb.userSubscription.findUnique({
+    where: {
+      userId: session?.user.id,
+    },
+  });
+
+  if (!userSubscription) {
+    return null;
+  }
+
+  return userSubscription;
 };
