83e7ab40197ab7877739baabd9c685d51a7c7ef1
bwestwood11 <dabrettwestwood@gmail.com>
2023-08-08 18:20:54 -0400

    typescript change

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -7,10 +7,12 @@ const DashboardLayout = async ({ children }: { children: React.ReactNode }) => {
   const apiLimitCount = await getApiLimitCount();
   const isPro = await checkSubscription()
 
+  const formattedApiLimitCount = typeof apiLimitCount === 'number' ? apiLimitCount : 0;
+
   return (
     <div className="h-full relative">
       <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 bg-gray-900">
-        <Sidebar isPro={isPro} apiLimitCount={apiLimitCount} />
+        <Sidebar isPro={isPro} apiLimitCount={formattedApiLimitCount} />
       </div>
       <main className="md:pl-72">
         <Navbar />
