6b43ea8eb15e4cda2f1fcc0edf409562d57a6ff0
bwestwood11 <dabrettwestwood@gmail.com>
2023-12-10 17:55:47 -0500

    changes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      }
+    ]
+  },
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.externals.push({
       sharp: "commonjs sharp",
