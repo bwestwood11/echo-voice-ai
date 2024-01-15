a0c5f9005ec2a6055976cdf5368b2ebe094b1a7e
bwestwood11 <dabrettwestwood@gmail.com>
2024-01-15 03:17:56 -0500

    change some env

--- lib/mail.ts
+++ lib/mail.ts
@@ -2,7 +2,7 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-const domain = process.env.NEXT_PUBLIC_APP_URL;
+const domain = process.env.SITE_URL || "http://localhost:3000";
 
 export const sendVerificationEmail = async (email: string, token: string) => {
   const confirmLink = `${domain}/auth/new-verification?token=${token}`;
